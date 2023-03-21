c0737f78b4ac5d114758af6786eeb4be6812fa96
Patchethium <asguftuikh@gmail.com>
2023-03-21 11:14:52 +0900

    add meta description

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,11 @@
 
 <svelte:head>
   <title>Anthe</title>
+  <meta
+    name="description"
+    content="Anthe is an out-of-box TTS software, 
+             fully controllable, free and open source"
+  />
 </svelte:head>
 <Header />
 <div class="w-full h-full">
